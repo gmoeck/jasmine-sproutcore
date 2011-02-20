0cbc7554ba4bce3922ef366e880e985e34d5ccf6
Gregory Moeck <gmoeck@gmail.com>
2011-02-19 21:55:17 -0800

    Add mouseover before clicking

--- jasmine-sproutcore.js
+++ jasmine-sproutcore.js
@@ -2,6 +2,7 @@ function clickOn(selector) {
   SC.RunLoop.begin();
   var target = SC.CoreQuery(selector);
   if(target.length == 0) throw new Error('Could not find ' + selector + ' on the page');
+  SC.Event.trigger(target, 'mousemove');
   SC.Event.trigger(target, 'mousedown');
   SC.Event.trigger(target, 'mouseup');
   SC.RunLoop.end();
