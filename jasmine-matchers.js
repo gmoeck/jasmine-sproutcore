a92951550345d238c991d54a9d61b6a5a7fce657
Gregory Moeck <gmoeck@gmail.com>
2011-04-29 17:19:47 -0700

    Change way matchers are added

--- jasmine-matchers.js
+++ jasmine-matchers.js
@@ -1,21 +1,26 @@
-jasmine.Matchers.prototype.toHaveClass = function(className) {
-  this.message = function() {
-    return [
-        "Expected object to have the '" + className + "' CSS class, but it did not",
-        "Expected object not to have the '" + className + "' CSS class, but it did"
-    ];
-  };
+beforeEach(function() {
+  this.addMatchers({
+    toHaveClass: function(className) {
+      this.message = function() {
+        return [
+            "Expected object to have the '" + className + "' CSS class, but it did not",
+            "Expected object not to have the '" + className + "' CSS class, but it did"
+        ];
+      };
 
-  return this.actual.hasClass(className);
-};
+      return this.actual.hasClass(className);
 
-jasmine.Matchers.prototype.toHaveContent = function(content) {
-  this.message = function() {
-    return [
-        "Expected page to have '" + content + "' within its content, but it did not",
-        "Expected page not to have '" + content + "' within its content, but it did"
-    ];
-  };
+    },
 
-  return this.actual.hasContent(content);
-};
+    toHaveContent: function(content) {
+      this.message = function() {
+        return [
+            "Expected page to have '" + content + "' within its content, but it did not",
+            "Expected page not to have '" + content + "' within its content, but it did"
+        ];
+      };
+
+      return this.actual.hasContent(content);
+    }
+  });
+});
--- jasmine-sproutcore.js
+++ jasmine-sproutcore.js
@@ -13,11 +13,11 @@ var JasmineSproutcore = {
     },
 
     within: function(selector, callback) {
-      SC.RunLoop.begin();
-      this.set('selector', selector);
-      SC.RunLoop.end();
       var page = this;
-      callback.apply(page, [page]);
+      runs(function() {
+        page.set('selector', selector);
+        callback.apply(page, [page]);
+      });
     },
   })
 };
